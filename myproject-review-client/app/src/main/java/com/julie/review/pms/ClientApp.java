197583a9f34cac1ac52760e49f7976b966beba77
julie0919 <julie.lee0919@gmail.com>
2021-04-06 21:40:32 +0900

    NetworkAPI - Clinet-Server Application

---
+++ myproject-review-client/app/src/main/java/com/julie/review/pms/ClientApp.java
@@ -0,0 +1,40 @@
+package com.julie.review.pms;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+
+
+public class ClientApp {
+
+  String serverAddress;
+  int port;
+
+  public static void main(String[] args) {
+
+    ClientApp app = new ClientApp("localhost", 8888);
+    app.execute();
+  }
+
+  public ClientApp(String serverAddress, int port) {
+    this.serverAddress = serverAddress;
+    this.port = port;
+  }
+
+  public void execute() {
+
+    try (Socket socket = new Socket(this.serverAddress, this.port);
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+        DataInputStream in = new DataInputStream(socket.getInputStream())) {
+
+      out.writeUTF("hello");
+      out.flush();
+
+      String response = in.readUTF();
+      System.out.println(response);
+
+    } catch (Exception e) {
+      System.out.println("서버와 통신하는 중에 오류 발생!");
+    }
+  }
+}
---
+++ myproject-review-server/app/src/main/java/com/julie/review/pms/ServerApp.java
@@ -0,0 +1,49 @@
+package com.julie.review.pms;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerApp {
+
+  int port;
+
+  public static void main(String[] args) {
+    ServerApp app = new ServerApp(8888);
+    app.service();
+  }
+
+  public ServerApp(int port) {
+    this.port = port;
+  }
+
+  private void service() {
+
+    // 클라이언트 연결을 기다리는 서버 소켓 생성
+    try (ServerSocket serverSocket = new ServerSocket(this.port)) {
+      System.out.println("서버 실행!");
+
+      processRequest(serverSocket.accept());
+    } catch (Exception e) {
+      System.out.println("서버 실행 중 오류 발생!");
+      e.printStackTrace();
+    }
+  }
+
+  private void processRequest(Socket socket) {
+    try (DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+        DataInputStream in = new DataInputStream(socket.getInputStream())) {
+
+      String request = in.readUTF();
+      System.out.println(request);
+
+      out.writeUTF("success");
+      out.flush();
+
+    } catch (Exception e) {
+      System.out.println("클라이언트의 요청을 처리하는 중에 오류 발생!");
+      e.printStackTrace();
+    }
+  }
+}
